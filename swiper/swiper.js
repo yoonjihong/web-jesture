92b2721eaed3b4cd1f081f7759da19ad57b54252
JiHong <47561303+yoonjihong@users.noreply.github.com>
2023-06-01 23:59:38 +0900

    feat: 코드정리

--- swiper/swiper.js
+++ swiper/swiper.js
@@ -6,8 +6,6 @@ const sideMenuSwiper = (target, onTouchEnd) => {
   let initialTranslateX; 
   let dir = null;
 
-  // test
-
   const touchStart = (e) => {
     [startX, startY] = [e.touches[0].clientX, e.touches[0].clientY];
     [currentX, currentY] = [startX, startY];
